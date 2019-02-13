90b548dd292d781f0b141ae912ca5d6ce62c71ba
wolfling-jim <wolgling.jim@gmail.com>
2019-02-13 17:40:35 +0800

    first commit
    first commit

---
+++ app/js/common.js
@@ -0,0 +1,13 @@
+$(function() {
+
+	$('#my-menu').mmenu({
+		extensions: [ 'widescreen', 'theme-black', 'effect-menu-slide', 'pagedim-black' ],
+		navbar: {
+			title: 'Elly'
+		},
+		offCanvas: {
+			position  : 'right'
+		}
+	});
+
+});
